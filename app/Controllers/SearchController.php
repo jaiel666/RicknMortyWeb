fe479b24184e792e8220539c56c34ac595b19495
jaiel666 <mgusts23@gmail.com>
2023-11-03 15:53:06 +0200

    first commit

---
+++ app/Controllers/SearchController.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use App\Api;
+use App\Response;
+
+class SearchController
+{
+    private Api $api;
+
+    public function __construct()
+    {
+        $this->api = new Api();
+    }
+
+    public function show(array $vars): Response
+    {
+        $episodeCode = $vars['id'];
+
+        $episode = $this->api->fetchEpisodeByCode($episodeCode);
+
+        $characters = $this->api->fetchCharactersForEpisode($episode->getId());
+
+        return new Response('episodes/show', [
+            'episode' => $episode,
+            'characters' => $characters,
+        ]);
+    }
+
+}
+
+
